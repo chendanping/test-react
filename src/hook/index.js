d274aaa8a3dff397f83f8439cb232d60ff49757e
Shirley Chen <shirly.chen@kreditme.id>
2019-07-05 09:19:27 +0800

    update

---
+++ src/hook/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+export default function Counter() {
+  const [count, setCount] = React.useState();
+  const [loading, setLoading] = React.useState(true);
+
+  const fetch = React.useCallback(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setCount(1);
+      setLoading(false);
+    }, 2000);
+  }, []);
+
+  React.useEffect(() => {
+    fetch();
+  }, [fetch]);
+  return (
+    <div>
+      {!loading ? (
+        <div>
+          Count: {count}
+          <button onClick={() => setCount(pre => pre + 1)}>+</button>
+          <button onClick={() => setCount(pre => pre - 1)}>-</button>
+        </div>
+      ) : (
+        <div>loading</div>
+      )}
+    </div>
+  );
+}
